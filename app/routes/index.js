8744071bb3c841473f9b7c7e883b007a75e1b474
Chung-Min Cheng <min427@gmail.com>
2017-06-12 20:21:27 +0900

    some updates for connecting firebase.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: () => {
-    // console.log(this);
-    // return this.store.findAll('social');
+    console.log(this.store);
+    // return this.store.findAll('chungmin');
   }
 });
