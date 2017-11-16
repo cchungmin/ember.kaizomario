ab25af43eb1bdec05548a4b657dc2588b8c2afe4
Chung Min Cheng <min427@gmail.com>
2017-11-16 14:28:11 +0900

    Style improvements.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,26 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+const Scrolling = Ember.Mixin.create({
+  bindScrolling: function(opts) {
+    let onScroll, _this = this;
+
+    onScroll = function() {
+      return _this.scrolled();
+    };
+
+    console.log('test');
+
+    // $(document).bind('touchmove', onScroll);
+    // $(window).bind('scroll', onScroll);
+  },
+
+  unbindScrolling: function() {
+    // $(window).unbind('scroll');
+    // $(document).unbind('touchmove');
+  }
+});
+
+export default Ember.Controller.extend(Scrolling, {
   classNameBindings: ['isActivated'],
   isActivated: true,
 
